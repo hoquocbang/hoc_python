5a19aa3dea0406c3632760ef1812afd645f41f2b
hoquocbang <bang.hoquoc@gmai.com>
2023-01-05 16:11:10 +0700

    day3 -update

--- buoi3/bai2.py
+++ buoi3/bai2.py
@@ -1,4 +1,10 @@
-print("a" > "b")
-print(3.0 > 3)
-print('' <= ' ')
-print(.5 > 1)
+#Dự đoán kết quả của các biểu thức so sánh sau:
+#'a' > 'b'
+#3.0 > 3
+#'' <= ' '
+#.5 > 1
+
+print("a" > "b") #False vì ta tra trong bảng mã ACSII ta thấy a = 97 và b = 98 nên a > b là sai
+print(3.0 > 3) #False vì giá trị 3.0 vẫn bằng 3
+print('' <= ' ') 
+print(.5 > 1) #False vì .5 là 0.5 mà 0.5 < 1 => False
--- buoi3/bai3.py
+++ buoi3/bai3.py
@@ -1,6 +1,16 @@
-print(0 and 1)
-print('' or None)
-print(3 and 4 or 0)
-print('a' or '1')
-print(not None)
-print(not 0)
+#Dự đoán kết quả của các biểu thức logic sau:
+#0 and 1
+#'' or None
+#3 and 4 or 0
+#'a' or '1'
+#not None
+#not 0
+
+print(bool())
+print(0 and 1) # 0 vì bool(0) = False mà trong and nếu giá trị đầu tiên là False thì sẽ trả về giá trị đầu là 0
+print('' or None) #None vì bool('') = False mà trong or nếu giá trị đầu tiên là False thì sẽ trả về giá trị thứ 2 là None
+print(3 and 4 or 0) # 4 vì bool(3) = True mà trong and nếu giá trị đầu tiên là True thì sẽ trả về giá trị 2 là 4
+                    # mà bool(4) = True mà trong or nếu giá trị đầu tiên là True thì sẽ trả về giá trị thứ 1 là 4
+print('a' or '1') # a vì bool('a') = True mà trong or nếu giá trị đầu tiên là True thì sẽ trả về giá trị thứ 1 là a
+print(not None) #True vì bool(None) = False mà not False  = True
+print(not 0) #True
